071a1f264cfb04c27d373c70b6b6d74b7b9f88e7
George Groh <george.groh@gmail.com>
2014-04-28 10:34:56 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FriendCircle::Application.config.secret_key_base = '904b8a8ac849a1e0f16e1fc17a8e6490b1828477d84324f5a75a3bc77bb8f7d5bf8ecf44b0dc9ca06608c1902a715b6004f6d9285bc8ec42e987945e4e1ce083'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FriendCircle::Application.config.session_store :cookie_store, key: '_FriendCircle_session'
